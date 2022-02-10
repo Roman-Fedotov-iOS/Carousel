449f5d41b8750b7a7d469aa582f183ef607ea625
Roman-Fedotov-iOS <romafed23@gmail.com>
2022-02-10 21:12:21 +0200

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Roman Fedotov on 10.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
